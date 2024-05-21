2fa3de81fe91633d045a4e00434901af4d0a16bc
manojadhikari22 <manojadhikari2207@gmail.com>
2024-05-21 09:53:37 +0200

    made all page responsive

--- src/js/service.js
+++ src/js/service.js
@@ -6,9 +6,9 @@ let servicesClass = document.querySelector(".services-page-info-grid");
 loader.style.display = "block";
 
 // Mapping of service names to image paths
-const imageMap = {
-  "Rickshaw Carry": "assets/Incline-Dumbbell-Hammer-Curl.jpg",
-  "Single-Leg Press": "assets/leg-press.jpg",
+/*const imageMap = {
+  "Rickshaw Carry": "../assets/Incline-Dumbbell-Hammer-Curl.jpg",
+  "Single-Leg Press": "../assets/leg-press.jpg",
   "Landmine twist": "assets/landmine-twist.jpg",
   "Weighted pull-up": "assets/weighted-pullup.jpg",
   "T-Bar Row with Handle": "assets/T-bar-row.jpg",
@@ -17,6 +17,18 @@ const imageMap = {
   "Dumbbell front raise to lateral raise": "dumbell-front-lat-raise.webp",
   "Clean from Blocks": "assets/clean-from-blocks.jpg",
   "Incline Hammer Curls": "assets/incline-hammer-curls.jpg",
+};*/
+const imageMap = {
+  "Rickshaw Carry": "https://res.cloudinary.com/node-blog/image/upload/v1715717338/dumbell-front-lat-raise_qol6jv.webp",
+  "Single-Leg Press": "https://res.cloudinary.com/node-blog/image/upload/v1715717338/leg-press_kmmx0y.jpg",
+  "Landmine twist": "https://res.cloudinary.com/node-blog/image/upload/v1715717625/landmine-twist_dkhefu.jpg",
+  "Weighted pull-up": "https://res.cloudinary.com/node-blog/image/upload/v1715717338/weighted-pullup_csao9e.jpg",
+  "T-Bar Row with Handle": "https://res.cloudinary.com/node-blog/image/upload/v1715717338/T-bar-row_kjjqv5.jpg",
+  "Palms-down wrist curl over bench": "https://res.cloudinary.com/node-blog/image/upload/v1715717338/palms-down_wrist_lc2jxf.jpg",
+  "Atlas Stones": "https://res.cloudinary.com/node-blog/image/upload/v1715717767/Atlas-stone_uqchtt.jpg",
+  "Dumbbell front raise to lateral raise": "https://res.cloudinary.com/node-blog/image/upload/v1715717338/dumbell-front-lat-raise_qol6jv.webp",
+  "Clean from Blocks": "https://res.cloudinary.com/node-blog/image/upload/v1715717819/clean-from-blocks_denzjv.jpg",
+  "Incline Hammer Curls": "https://res.cloudinary.com/node-blog/image/upload/v1715717338/incline-hammer-curls_jbcc1i.jpg",
 };
 
 // Define an array of fitness services
@@ -105,69 +117,5 @@ searchInput.addEventListener("keyup", (e) => {
   });
 });
 
-/*const starWarsList = document.querySelector('#services-page-info-grid');
 
-const filmImages = {
-  "Rickshaw Carry": "assets/Incline-Dumbbell-Hammer-Curl.jpg",
-  "Single-Leg Press": "assets/leg-press.jpg",
-  "Landmine twist": "assets/landmine-twist.jpg",
-  "Weighted pull-up": "assets/weighted-pullup.jpg",
-  "T-Bar Row with Handle": "assets/T-bar-row.jpg",
-  "Palms-down wrist curl over bench": "assets/palms-down_wrist.jpeg",
-  "Atlas Stones": "assets/Atlas-stone.jpg",
-  "Dumbbell front raise to lateral raise": "dumbell-front-lat-raise.webp",
-  "Clean from Blocks": "assets/clean-from-blocks.jpg",
-  "Incline Hammer Curls": "assets/incline-hammer-curls.jpg",
-};
-
-const fetchFitnessServices = async () => {
-  const response = await fetch("https://api.api-ninjas.com/v1/exercises", {
-    headers: { "X-Api-Key": "CmULi2yb02XRvTpr+FRwvQ==YXJ1uYD0iPxaF1rP" },
-    contentType: "application/json",
-  });
-  const data = await response.json();
-  return data;
-};
-
-const renderFitnessServices = (services) => {
-  services.forEach((service) => {
-    const serviceDiv = document.createElement("div");
-    serviceDiv.classList.add("services-list-item");
-    serviceDiv.innerHTML = `
-      <h3>${service.name}</h3>
-      <p>Type: ${service.type}</p>
-      <p>Muscle: ${service.muscle}</p>
-      <p>Equipment: ${service.equipment}</p>`;
-    const image = document.createElement("img");
-    const imageUrl = filmImages[service.name];
-    if (imageUrl) {
-      image.src = imageUrl;
-    }
-    serviceDiv.appendChild(image);
-    starWarsList.appendChild(serviceDiv);
-  });
-};
-
-const searchInput = document.getElementById("searchInput");
-
-const filterServices = (services, searchString) => {
-  return services.filter(
-    (service) =>
-      service.name.toLowerCase().includes(searchString) ||
-      service.type.toString().includes(searchString)
-  );
-};
-
-const updateServices = async () => {
-  const searchString = searchInput.value.toLowerCase();
-  const services = await fetchFitnessServices();
-  const filteredServices = filterServices(services, searchString);
-  starWarsList.innerHTML = "";
-  renderFitnessServices(filteredServices);
-};
-
-searchInput.addEventListener("keyup", updateServices);
-
-// Initial load
-updateServices();*/
 
